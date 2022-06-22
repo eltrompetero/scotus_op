2fde1814ac66cc373fed7486137504e0c1a03412
Eddie Lee <edlee@csh.ac.at>
2022-06-22 09:50:47 +0200

    added pp.split_by_sub_op() and .standardize_header()

--- scotus_op/preprocess.py
+++ scotus_op/preprocess.py
@@ -37,6 +37,63 @@ def extract_plain_txt():
             with open(f'{DATADR}/extracted_text/{f.split(".")[0]}.txt', 'w') as fout:
                 fout.write(out_text)
 
+def split_by_sub_op(text):
+    """Split given opinion text by start of dissenting or concurring opinions using a
+    simple lookup table.
+
+    Assumes that concurrences come before dissents.
+
+    Parameters
+    ----------
+    text : str
+
+    Returns
+    -------
+    list of str
+    """
+    
+    # put into list of text split into sections
+    text = re.split(r'(\n        [ ]+.*(?!\n.*)concurring.*\n)', text)
+    if len(text)==1:
+        return text
+
+    # combine sections that all belong to the same concurrence
+    # 0th item is majority op, so start looking for concurrences afterwards
+    conc_text = [text[1]]
+    # standardize sub-op header for matching later
+    conc_head = standardize_header(text[1])
+    for i in range((len(text)-1)//2):
+        if conc_head==standardize_header(text[i*2+1]):
+            conc_text[-1] += text[(i+1)*2]
+        else:
+            conc_head = ''.join(text[i*2+1].split())
+            conc_text.append(text[(i+1)*2])
+    text = text[:1]
+    text.extend(conc_text)
+
+    # same process for dissents (presuming they come after)
+    dtext = re.split(r'(\n        [ ]+.*(?!\n.*)dissenting.*\n)', text[-1])
+    if len(dtext)==1:
+        return text
+
+    # combine sections that all belong to the same concurrence
+    # 0th item is majority op, so start looking for concurrences afterwards
+    dis_text = [dtext[1]]
+    dis_head = standardize_header(dtext[1])
+    for i in range((len(dtext)-1)//2):
+        if dis_head==standardize_header(dtext[i*2+1]):
+            dis_text[-1] += dtext[(i+1)*2]
+        else:
+            dis_head = ''.join(dtext[i*2+1].split())
+            dis_text.append(dtext[(i+1)*2])
+    dtext = dtext[:1]
+    text.extend(dis_text)
+
+    return text
+
+def standardize_header(s):
+    return ''.join(s.split()).replace(',', '').replace('.', '')
+
 def preprocess_plain_txt_op(text):
     """Clean up plain text and turn it into sentences.
 
